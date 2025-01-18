ff5996bcc1194d224591021a0a38bda502e17c79
Angel Penchev <angel.penchev.tsb@gmail.com>
2025-01-18 18:24:32 +0200

    fix: Update DevHubOne Guild GitHub milestones
    Updated the GitHub milestone of 2 repositories to end with 2025 Feb H2.

--- terraform/github/devhubone_guild/milestones.tf
+++ terraform/github/devhubone_guild/milestones.tf
@@ -3,7 +3,7 @@ resource "github_repository_milestone" "devhubone_guild_feature_completeness_mil
   repository  = github_repository.repository.name
   title       = "DevHubOne Guild Feature Completeness"
   description = "Complete the main functionalities of the DevHubOne Guild platform."
-  due_date    = "2024-12-01"
+  due_date    = "2025-03-01"
   state       = "open"
 }
 
--- terraform/github/devhubone_infrastructure/milestones.tf
+++ terraform/github/devhubone_infrastructure/milestones.tf
@@ -3,6 +3,6 @@ resource "github_repository_milestone" "devhubone_guild_base_infrastructure_conf
   repository  = github_repository.repository.name
   title       = "DevHubOne Guild Base Infrastructure Configuration"
   description = "Complete describing existing infrastructure using Terraform, set up automated deployments, secret managers, etc."
-  due_date    = "2024-12-01"
+  due_date    = "2025-03-01"
   state       = "open"
 }
